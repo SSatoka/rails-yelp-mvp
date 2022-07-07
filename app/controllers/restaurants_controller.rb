8bc42835a559e0f5658ad1c517d63afdeb7f3170
Satoka Sotome <satoka121@gmail.com>
2022-07-07 20:20:26 +0900

    Rails new with frontend and form gems

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,21 +4,21 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
+
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @review = Review.find(params[:review_id])
+    @restaurant.review = @review
+    redirect_to restaurant_path(@restaurant)
   end
 
   def edit
-  end
-
-  def update
-  end
-
-  def destroy
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,22 @@
 class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    # associating review with a corresponding restaurant
+    @restaurant.review = @review
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    # associating review with a corresponding restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant.review = @review
+    @review.save
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
